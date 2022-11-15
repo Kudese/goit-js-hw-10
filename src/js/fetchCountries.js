e75171a39d9283814d8e2fbc1fc6b839394b869c
Kudese <d.kuprichenkov@gmail.com>
2022-11-15 14:56:02 +0200

    task1

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,13 @@
+import Notiflix from "notiflix";
 export default function fetchCountries(name) {
-    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-    .then((response)=> {
-      return  response.json()
-    } )
-    .then((data)=>{  const [name, capital,population,flags,languages]= data
-     return name, capital,population,flags,languages})
-
-
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(notFuandError =>(Notiflix.Notify.failure('Oops, there is no country with that name')))
+}
